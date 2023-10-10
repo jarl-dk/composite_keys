544e794cf010186eb9e6203c5ec53a38c930c673
Jarl Friis <jarl@softace.dk>
2023-10-10 10:06:56 +0200

    spec for book

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "infers primary key" do
+    expect(described_class.primary_key).to eq "number"
+  end
+
+  context "valid book" do
+    subject(:book) { described_class.new(number: 1, title: "First book") }
+
+    it { is_expected.to be_valid }
+
+    it "can save" do
+      expect { book.save! }.not_to raise_exception
+    end
+
+    context "is saved" do
+      before { book.save! }
+
+      it "can be found" do
+        expect(described_class.find(1)).to eq book
+      end
+    end
+  end
 end
